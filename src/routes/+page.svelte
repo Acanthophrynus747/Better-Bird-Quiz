6f3c11809c93f9f500115c6bca1afabe003fa0b1
Acanthophrynus747 <jacksbuilds@gmail.com>
2025-07-15 01:53:18 -0700

    messing around

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Someday there will be a bird quiz here</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Hell World</p>
+<button>
+    bird one
+</button>
+<br>
+<button>
+    bird two
+</button>
+<br>
+<button>
+    bird three
+</button>
